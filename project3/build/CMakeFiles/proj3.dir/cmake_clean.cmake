fd7d9b640c09ce125565839e53ce43546a24a7bb
melanie-wiegand <mwiegan6@uoregon.edu>
2026-04-19 16:52:40 -0700

    t1 & t2

---
+++ project3/build/CMakeFiles/proj3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj3.dir/proj3.cxx.o"
+  "CMakeFiles/proj3.dir/proj3.cxx.o.d"
+  "proj3"
+  "proj3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
